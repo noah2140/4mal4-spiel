2b78ee0d8f25f8642dc484ddb9f4aa3ef9351d28
Noah Ickenroth <noahickenroth@uni-koblenz.de>
2024-12-10 20:18:09 +0100

    Added puzzles

--- src/assets/puzzles.ts
+++ src/assets/puzzles.ts
@@ -13,10 +13,46 @@ export const puzzles: Puzzle[] = [
     {
         date: '2024-12-11',
         categories: [
-            { name: '', words: ['', '', '', ''] },
-            { name: '', words: ['', '', '', ''] },
+            { name: 'Bei Kurvendiskussionen gesehen', words: ['Ableitung', 'Monotonie', 'Symmetrie', 'Wendepunkt'] },
+            { name: 'Blautöne', words: ['Eis', 'Himmel', 'Ozean', 'Stahl'] },
             { name: 'Teile einer Gitarre', words: ['Hals', 'Kopf', 'Korpus', 'Saite'] },
+            { name: 'Wasser_', words: ['Fall', 'Leitung', 'Melone', 'Stoff'] },
+        ],
+    },
+    {
+        date: '2024-12-12',
+        categories: [
+            { name: 'Teile eines Schiffs', words: ['Brücke', 'Deck', 'Kiel', 'Rumpf'] },
+            { name: 'Saugen etwas auf', words: ['Reis', 'Schwamm', 'Staubsauger', 'Windel'] },
+            { name: 'In Harry Potter Titeln', words: ['Kammer', 'Kelch', 'Orden', 'Stein'] },
+            { name: 'Obst_', words: ['Baum', 'Garten', 'Plantage', 'Stand'] },
+        ],
+    },
+    {
+        date: '2024-12-13',
+        categories: [
+            { name: 'Stellt man sich vor', words: ['Illusion', 'Traum', 'Utopie', 'Wunsch'] },
+            { name: 'Geometrische Begriffe', words: ['Gerade', 'Punkt', 'Pyramide', 'Würfel'] },
+            { name: 'Typische Abteilungen in Buchhandlungen', words: ['Fantasie', 'Historisch', 'Krimi', 'Reisen'] },
+            { name: 'Beginnen mit W-Fragewörtern', words: ['Wasser', 'Werben', 'Wieder', 'Woche'] },
+        ],
+    },
+    {
+        date: '2024-12-14',
+        categories: [
+            { name: '', words: ['', '', 'Pin', 'Tan'] },
             { name: '', words: ['', '', '', ''] },
+            { name: '', words: ['', '', '', ''] },
+            { name: 'Dia_', words: ['Dem', 'Kon', 'Log', 'Show'] },
+        ],
+    },
+    {
+        date: '2024-12-18',
+        categories: [
+            { name: 'Toll', words: ['Exzellent', 'Phänomenal', 'Prima', 'Super'] },
+            { name: 'Arche Noah - nahe Begriffe', words: ['Boot', 'Flut', 'Noah', 'Tier'] },
+            { name: 'Palindrome', words: ['Ebbe', 'Ehe', 'Hannah', 'Rentner'] },
+            { name: 'Anagramme von Monaten', words: ['Mia', 'Pilar', 'Saugut', 'Urfarbe'] },
         ],
     },
     {
