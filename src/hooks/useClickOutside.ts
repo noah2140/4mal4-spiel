9008df77acc6151955dcf69512f0fb74f922ac72
Noah Ickenroth <noahickenroth@uni-koblenz.de>
2024-12-11 12:04:39 +0100

    Small code cleanup

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 const useClickOutside = (ref: React.RefObject<HTMLElement | null>, onClose: () => void) => {
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
-            // Check if ref.current exists and doesn't contain the event target
             if (ref.current && !ref.current.contains(event.target as Node)) {
                 onClose();
             }
@@ -11,7 +10,6 @@ const useClickOutside = (ref: React.RefObject<HTMLElement | null>, onClose: () =
 
         document.addEventListener('mousedown', handleClickOutside);
 
-        // Cleanup the event listener when the component is unmounted
         return () => {
             document.removeEventListener('mousedown', handleClickOutside);
         };
